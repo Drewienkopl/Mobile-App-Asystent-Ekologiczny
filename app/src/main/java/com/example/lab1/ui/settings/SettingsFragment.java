12612c75d88ee1206f31423caca84eac980aabfa
Drewienkopl <hubert.drewienko@gmail.com>
2025-10-28 23:35:21 +0100

    Sending Files

---
+++ app/src/main/java/com/example/lab1/ui/settings/SettingsFragment.java
@@ -0,0 +1,43 @@
+package com.example.lab1.ui.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.example.lab1.databinding.FragmentSettingsBinding;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    private FragmentSettingsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SettingsViewModel settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSettings;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
